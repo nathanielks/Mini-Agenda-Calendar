ffd17ba43f37a5199e4314c4e076bb73f533164f
Nathaniel Schweinberg <nathaniel@fightthecurrent.org>
2012-08-15 16:01:26 -0500

    Updated Screenshot

--- functions.php
+++ functions.php
@@ -1,14 +1,14 @@
 <?php
 
 
-function joe_dimatteo_calendar_scripts_styles() {
-	$joe_dimatteo_calendar_url = apply_filters(
+function mini_agenda_calendar_scripts_styles() {
+	$mini_agenda_calendar_url = apply_filters(
 		'ai1ec_template_root_url',
-		apply_filters( 'ai1ec_template', 'joe_dimatteo_calendar' )
+		apply_filters( 'ai1ec_template', 'mini_agenda_calendar' )
 	);
 
-	wp_enqueue_style( 'joe_dimatteo_calendar-style', "$joe_dimatteo_calendar_url/css/joe-dimatteo-calendar.css" );
-	wp_enqueue_script( 'joe_dimatteo_calendar-scripts', "$joe_dimatteo_calendar_url/js/joe-dimatteo-calendar.js", array('jquery') );
+	wp_enqueue_style( 'mini_agenda_calendar-style', "$mini_agenda_calendar_url/css/mini-agenda-calendar.css" );
+	wp_enqueue_script( 'mini_agenda_calendar-scripts', "$mini_agenda_calendar_url/js/mini-agenda-calendar.js", array('jquery') );
 }
-add_action( 'wp_print_styles', 'joe_dimatteo_calendar_scripts_styles' );
+add_action( 'wp_print_styles', 'mini_agenda_calendar_scripts_styles' );
 
